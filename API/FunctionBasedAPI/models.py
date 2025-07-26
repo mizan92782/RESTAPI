229c9de0647f113e31ff7467dc6d073494588205
mizanur rahman <mizanmd92782@gmail.com>
2025-07-26 10:19:35 +0600

    serializer data validation

--- API/FunctionBasedAPI/models.py
+++ API/FunctionBasedAPI/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    institute = models.CharField(default='PCIU',max_length=100)
+    location = models.CharField(default='Bd',max_length=100)
     department = models.CharField(max_length=20)
 
     def __str__(self):
--- API/FunctionBasedAPI/serializer.py
+++ API/FunctionBasedAPI/serializer.py
@@ -5,3 +5,22 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+        
+        #! read only filed not show but add
+        read_only_fields = ['institute']
+        #! write only field can write but not show in get
+        extra_kwargs = {
+            'location': {'write_only': True},
+            
+        }
+    
+    def validate(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        if data['age'] < 18:
+            raise serializers.ValidationError("Children not allowed")
+        
+        if data['name'][0].islower():
+            raise serializers.ValidationError("Fitst word can not be in Uppercase")
+        return data
